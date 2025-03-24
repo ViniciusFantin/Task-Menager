8253d91f88e4f254005a25523458f135a9b7c0cf
Vinicius Fantin <vinifantin302@gmail.com>
2025-03-24 15:30:50 -0300

    first commit

---
+++ to-do-list/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className={styles.footer}>
+        <p>
+          <span>TaskMenager</span> @ 2025
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
