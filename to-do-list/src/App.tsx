896342f1de0838448636f0a62ca65738fed4b5d7
Vinicius Fantin <vinifantin302@gmail.com>
2025-03-25 16:21:13 -0300

    New - modal = edit/delete

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -11,6 +11,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
+import Modal from "./components/Modal";
 
 function App() {
   const [taskList, setTaskList] = useState<ITask[]>([]);
@@ -23,8 +24,24 @@ function App() {
     );
   };
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (): void => {
+    hideOrShowModal(true);
+  };
+
   return (
-    <div className="App">
+    <div>
+      <Modal
+        children={<TaskForm btnText="Editar Tarefa" taskList={taskList} />}
+      />
       <Header />
       <main className={styles.main}>
         <div>
@@ -37,7 +54,11 @@ function App() {
         </div>
         <div>
           <h2>your Tasks: </h2>
-          <TaskList taskList={taskList} handleDelete={deleteTask} />
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
         </div>
       </main>
       <Footer />
